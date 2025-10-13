c7131946e7d3e73acf108706a62ad29338140ccf
Saumya <76719334+Saumya1722@users.noreply.github.com>
2025-10-13 22:08:24 +0530

    JS- day 1
    variables, operators, (var, let, constant),loops

---
+++ Back to JS/JS_day_1/index.js
@@ -0,0 +1,144 @@
+/* line below used to print on console window */
+// console.log('Saumya');
+// variable
+// let a=5;
+// console.log(a);
+
+// let b='Saumya';
+// console.log(b);
+
+// let c=true;
+// console.log(c);
+
+
+
+// if(true)
+// {
+//     let a=5;
+//     let a=10;
+//     console.log(a);
+// }
+// console.log(a);
+
+// let a=10;
+// console.log(a);
+
+// if(true)
+// {
+//     var a=5;
+//     var a=10;
+//     console.log(a);
+// }
+
+// if(true)
+// {
+//     var a=10;
+//     console.log(a);
+// }
+
+// if(true)
+// {
+//     var a=10;
+    
+// }
+// console.log(a);
+
+// const num=10;
+// num=15;
+
+// dynamic typing
+// let firstName='Satyam';
+// console.log(firstName);
+
+// firstName=5;
+// console.log(firstName);
+
+// firstName=true;
+// console.log(firstName);
+
+// Comparison Operator
+// let str='1';
+// let num=1;
+
+// let result=(str==num);
+// console.log(result);
+
+// let result=(str===num);
+// console.log(result);
+
+// let num1=10;
+// let num2=12;
+
+// console.log(num1>num2);
+// console.log(num1<num2);
+// console.log(num1>=num2);
+// console.log(num1<=num2);
+// console.log(num1!==num2);
+// console.log(num1===num2);
+
+// Ternary operator
+
+// let age=47;
+// let status=(age>=18)?"Vote":"No Vote";
+// console.log(status);
+
+// IF- else case
+// let marks=95;
+// if(marks>=90)
+// {
+//     console.log('A');
+// }
+// else if(marks>=80)
+// {
+//     console.log('B');
+// }
+// else if(marks>=70)
+// {
+//     console.log('C');
+// }
+// else if(marks<=50)
+// {
+//     console.log('E');
+// }
+
+// switch case
+// let num=1;
+
+// switch(num)
+// {
+//     case 1:console.log('A');
+//     // break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;
+//     default:console.log('D');
+// }
+
+// for loop
+// for(let i=1;i<=5;i=i+1)
+// {
+// console.log('Saumya'+i);
+// }
+
+// for(let i=0;i<5;i=i+1)
+// {
+// console.log('Saumya'+i);
+// }
+
+// while loop
+// let x=1;
+// while(x<=5)
+// {
+//     console.log(x);
+//     x++;
+// }
+
+// do-while loop
+let a=1;
+do
+{
+    console.log(a);
+    a++;
+}
+while(a<10)
